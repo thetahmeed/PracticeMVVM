9e8d9c4dc38c56307951f5d77754412bd89ff005
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-18 20:59:11 +0600

    Complete SignIn > ViewModel

--- app/src/main/java/com/tahmeedul/practicemvvm/repository/SignInRepository.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/repository/SignInRepository.java
@@ -1,7 +1,12 @@
 package com.tahmeedul.practicemvvm.repository;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.tahmeedul.practicemvvm.model.SignInModel;
@@ -25,6 +30,26 @@ public class SignInRepository {
         }
 
         return isAuthenticateLiveData;
+    }
+
+    public MutableLiveData<String> firebaseSignInWithGoogle(AuthCredential authCredential){
+        MutableLiveData<String> mutableLiveData = new MutableLiveData<>();
+
+        firebaseAuth.signInWithCredential(authCredential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                String uid = currentUser.getUid();
+                mutableLiveData.setValue(uid);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mutableLiveData.setValue(e.toString());
+            }
+        });
+
+        return mutableLiveData;
     }
 
 }
--- app/src/main/java/com/tahmeedul/practicemvvm/viewmodel/SignInViewModel.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/viewmodel/SignInViewModel.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import com.google.firebase.auth.AuthCredential;
 import com.tahmeedul.practicemvvm.model.SignInModel;
 import com.tahmeedul.practicemvvm.repository.SignInRepository;
 
@@ -13,18 +14,19 @@ public class SignInViewModel extends AndroidViewModel {
 
     private SignInRepository signInRepository;
     public LiveData<SignInModel> checkAuthenticateLiveData;
+    public LiveData<String> authenticationLiveData;
 
     public SignInViewModel(@NonNull Application application) {
         super(application);
-
         signInRepository = new SignInRepository();
+    }
 
+    public void signInWithGoogle(AuthCredential authCredential){
+        authenticationLiveData = signInRepository.firebaseSignInWithGoogle(authCredential);
     }
 
     public void checkAuthenticate(){
-
         checkAuthenticateLiveData = signInRepository.checkAuthenticationFirebase();
-
     }
 
 }
