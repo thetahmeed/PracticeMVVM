4f2f6ed06c2b64f738a986b5dceaaa97c9da9628
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-18 16:43:42 +0600

    Created sign in activity

---
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.tahmeedul.practicemvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tahmeedul.practicemvvm.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
