43b2708165f571fc8b603379afcb18dad90c4e2d
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-18 16:42:21 +0600

    created new activity called SplashActivity without any layout. It will just check the user is logged in or not

---
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.tahmeedul.practicemvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
