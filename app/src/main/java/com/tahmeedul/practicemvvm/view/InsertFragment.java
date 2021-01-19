d8df3e0c5dfad6417ebb416ad64c88860d16165f
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-19 21:26:10 +0600

    added connection to InsertFragment to her layout's elements

--- app/src/main/java/com/tahmeedul/practicemvvm/view/InsertFragment.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/InsertFragment.java
@@ -1,16 +1,26 @@
 package com.tahmeedul.practicemvvm.view;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.tahmeedul.practicemvvm.R;
 
 public class InsertFragment extends Fragment {
+
+    CircularImageView newImage;
+    EditText newName, newPhone, newEmail;
+    Button newButton;
 
     public InsertFragment() {
         // Required empty public constructor
@@ -22,4 +32,32 @@ public class InsertFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_insert, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        newImage = view.findViewById(R.id.userImageViewId);
+        newName = view.findViewById(R.id.newNameId);
+        newPhone = view.findViewById(R.id.newPhoneId);
+        newEmail = view.findViewById(R.id.newEmailId);
+        newButton = view.findViewById(R.id.newSaveButtonId);
+
+        newImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+
 }
