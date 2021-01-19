331cb71f149d9d4fe83358517bfd9248a7b5df77
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-19 16:03:16 +0600

    Bottom Navigation:
    8. Complete the bottom navigation

--- app/src/main/java/com/tahmeedul/practicemvvm/view/MainActivity.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/MainActivity.java
@@ -1,16 +1,28 @@
 package com.tahmeedul.practicemvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tahmeedul.practicemvvm.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationId);
+        navController = Navigation.findNavController(this, R.id.navHostFragmentId);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
