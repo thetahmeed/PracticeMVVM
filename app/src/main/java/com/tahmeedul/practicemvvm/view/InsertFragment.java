e2602180107cfcae5d9a51bad3a6193502aa1fc9
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-19 21:55:09 +0600

    added Selecting image > cropping image > setting image on newImage

--- app/src/main/java/com/tahmeedul/practicemvvm/view/InsertFragment.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/InsertFragment.java
@@ -3,6 +3,7 @@ package com.tahmeedul.practicemvvm.view;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -17,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.tahmeedul.practicemvvm.R;
@@ -28,6 +30,7 @@ public class InsertFragment extends Fragment {
     CircularImageView newImage;
     EditText newName, newPhone, newEmail;
     Button newButton;
+    Uri selectedImageUri;
 
     public InsertFragment() {
         // Required empty public constructor
@@ -44,7 +47,7 @@ public class InsertFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        newImage = view.findViewById(R.id.userImageViewId);
+        newImage = view.findViewById(R.id.newImageViewId);
         newName = view.findViewById(R.id.newNameId);
         newPhone = view.findViewById(R.id.newPhoneId);
         newEmail = view.findViewById(R.id.newEmailId);
@@ -84,5 +87,33 @@ public class InsertFragment extends Fragment {
         }
     }
 
+    private void pickImage() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(getContext(), this);             //.start(getActivity()); {Will check it letter}
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == getActivity().RESULT_OK){
+                // got the uri
+                selectedImageUri = result.getUri();
+                // set selected image on the newImage
+                newImage.setImageURI(selectedImageUri);
+            }else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception exception = result.getError();
+                Toast.makeText(getActivity(), "Error: "+exception.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(getActivity(), "Image selection failed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
 }
