7b52e54a3acc2b951a58fb8f297b812e9dae0bda
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-20 18:41:12 +0600

    Complete Repository for Getting Contacts

--- app/src/main/java/com/tahmeedul/practicemvvm/repository/NewContactRepository.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/repository/NewContactRepository.java
@@ -1,20 +1,27 @@
 package com.tahmeedul.practicemvvm.repository;
 
 import android.net.Uri;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.tahmeedul.practicemvvm.model.NewContactModel;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NewContactRepository {
@@ -51,8 +58,8 @@ public class NewContactRepository {
                         newUserAllData.put("image", imageUrl.toString());
 
                         // Pushing all info to the database
-                        firebaseFirestore.collection("ContactsList").document(uid)
-                                .collection("User").document(newContactModel.getNewId()).set(newUserAllData)
+                        firebaseFirestore.collection("User").document(uid)
+                                .collection("ContactsList").document(newContactModel.getNewId()).set(newUserAllData)
                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
                                     public void onSuccess(Void aVoid) {
@@ -76,6 +83,46 @@ public class NewContactRepository {
 
 
         return insertResultLiveData;
+    }
+
+    // Getting data
+    public MutableLiveData<List<NewContactModel>> gettingContactsList(){
+        MutableLiveData<List<NewContactModel>> allSavedContacts = new MutableLiveData<>();
+
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        List<NewContactModel> contactsList = new ArrayList<>();
+
+        // We will got the data on 'task'
+        firebaseFirestore.collection("User").document(uid)
+                .collection("ContactsList").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                contactsList.clear();   // Clear the previous contact list
+
+                for (DocumentSnapshot documentSnapshot : task.getResult()){
+                    String id = documentSnapshot.getString("id");
+                    String name = documentSnapshot.getString("name");
+                    String phone = documentSnapshot.getString("phone");
+                    String email = documentSnapshot.getString("email");
+                    String image = documentSnapshot.getString("image");
+
+                    // 1 Getting each Contact
+                    NewContactModel newContactModel = new NewContactModel(id, name, phone, email, image);
+                    // 2 Adding then to the list
+                    contactsList.add(newContactModel);
+                }
+                // 3 Adding the list to MutableLiveData
+                allSavedContacts.setValue(contactsList);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+
+        return allSavedContacts;
     }
 
 }
