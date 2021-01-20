7f4fe60b48c52bb8332af91fcbf78849bbba46ab
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-20 19:09:07 +0600

    Complete View for Getting Contacts

--- app/src/main/java/com/tahmeedul/practicemvvm/view/ListFragment.java
+++ app/src/main/java/com/tahmeedul/practicemvvm/view/ListFragment.java
@@ -2,15 +2,26 @@ package com.tahmeedul.practicemvvm.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.tahmeedul.practicemvvm.R;
+import com.tahmeedul.practicemvvm.model.NewContactModel;
+import com.tahmeedul.practicemvvm.viewmodel.NewContactsViewModel;
+
+import java.util.List;
 
 public class ListFragment extends Fragment {
+
+    private NewContactsViewModel newContactsViewModel;
 
     public ListFragment() {
         // Required empty public constructor
@@ -19,7 +30,35 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        inNewContactViewModel();
+        setUpRecycle();
+
+
+    }
+
+    private void setUpRecycle() {
+        newContactsViewModel.getData();
+        newContactsViewModel.allContacts.observe(getViewLifecycleOwner(), new Observer<List<NewContactModel>>() {
+            @Override
+            public void onChanged(List<NewContactModel> newContactModels) {
+                if (newContactModels.size() != 0){
+                    String name = newContactModels.get(0).getNewName();
+                    Toast.makeText(getActivity(), ""+name, Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getActivity(), "No contacts found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void inNewContactViewModel() {
+        newContactsViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(getActivity().getApplication())).get(NewContactsViewModel.class);
     }
 }
